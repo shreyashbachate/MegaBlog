0255c32e2a5f2b5850d80665a7acb8b059e2003c
shreyashbachate <=>
2024-03-01 14:57:08 +0530

    added authorization layout

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+export default function Protected({ children, authentication = true }) {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authentication, navigate, authStatus]);
+
+  return loader ? <h1 className="text-green-500 text-center font-bold"> Loading </h1> : <>{children}</>
+}
