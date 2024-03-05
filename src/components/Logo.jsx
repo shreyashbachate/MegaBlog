4061cc57d015565ddd119abd1cfad84d6299ed22
shreyashbachate <=>
2024-03-05 16:41:15 +0530

    added image in logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,8 +2,15 @@ import React from "react";
 
 function Logo({ width = "100px" }) {
   return (
-    <img src="" alt="" />
-  )
+    <div className="text-center">
+      <img
+        src="https://cdn-icons-png.flaticon.com/512/1809/1809216.png"
+        width={width || 50}
+        height={50}
+        alt="React Post Logo"
+      />
+    </div>
+  );
 }
 
 export default Logo;
