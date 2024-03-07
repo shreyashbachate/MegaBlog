f6768ee87cdcc26fd0a29a1a0cedc6f920e2be0b
shreyashbachate <=>
2024-03-07 15:19:19 +0530

    added styles

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -27,7 +27,7 @@ function Login() {
   };
 
   return (
-    <div className="flex items-center justify-center w-full m-2 p-2">
+    <div className="flex items-center justify-center m-2 p-2">
       <div
         className={`mx-auto w-full max-w-lg bg-[#2a3347] rounded-xl p-10 border border-black/10`}
       >
